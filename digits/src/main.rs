8d1daef4bfcd76da844206a1d113a9c6d51529c0
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2025-01-24 15:27:22 -0800

    digits: read file, split lines by ,

--- digits/src/main.rs
+++ digits/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("starting");
+    let contents =
+        fs::read_to_string("sample.csv")
+            .expect("Failed to read file");
+
+    let file_lines = contents.lines();
+    for line in file_lines {
+        let blocks = line.split(',');
+        for block in blocks {
+            println!("{block}");
+        }
+        println!("{line}");
+    }
+
+    println!("finished");
 }
