6d0cc85912eb2be37f444f23873d570de9a0e5dd
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2025-01-24 16:30:37 -0800

    begin extracting example parser
    goal: rewrite as a map

--- digits/src/main.rs
+++ digits/src/main.rs
@@ -6,6 +6,27 @@ struct Example {
     pixels: Vec<u8>
 }
 
+fn parse_example (line: &str) {
+    let blocks = line.split(',');
+    let mut numbers: Vec<u8> = Vec::new();
+
+    for block in blocks {
+        let n =
+            block
+                .parse::<u8>()
+                .expect("Failed to convert to number");
+        numbers.push(n);
+    }
+
+    let label = numbers[0];
+    let pixels: Vec<u8> = numbers[1..].to_vec();
+
+    Example {
+        label: label,
+        pixels: pixels
+    };
+    }
+
 fn main() {
     println!("starting");
 
@@ -15,7 +36,17 @@ fn main() {
 
     let mut sample: Vec<Example> = Vec::new();
 
+    // TODO: rewrite as a map
+    // let file_lines = contents.lines();
+    // let test =
+    //     file_lines
+    //         .skip(1)
+    //         .map(parse_example);
+
     let file_lines = contents.lines();
+    // skip 1 line (the headers)
+    let file_lines = file_lines.skip(1);
+
     for line in file_lines {
 
         let blocks = line.split(',');
